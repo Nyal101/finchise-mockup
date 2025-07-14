093099a3d97664cd27bfbaa41872a474ac3a50f6
Nyal101 <nyal.patel9@icloud.com>
2025-07-14 15:50:11 +0100

    fix journals

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
+
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
@@ -59,10 +59,7 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: (props) => <ChevronLeft className={cn("size-4")} {...props} />,
-        IconRight: (props) => <ChevronRight className={cn("size-4")} {...props} />,
-      }}
+
       {...props}
     />
   )
